73dccced2901c8a94a0bcbfb577177d0da8125af
Yulian <stryulvas@gmail.com>
2022-09-19 14:56:23 +0300

    main

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,15 @@
 'use strict'
 $( document ).ready(
 	function() {
-		$(document).ready(function () {
-			$(".multiple-items").slick({
+		$( document ).ready( function () {
+			$( '.multiple-items' ).slick({
 				speed: 500,
 				slidesToShow: 3,
 				slidesToScroll: 3,
-				nextArrow: '<button type="button" class="slick-next"></button>',
-				prevArrow: '<button type="button" class="slick-prev"></button>'
+				// eslint-disable-next-line max-len
+				nextArrow: '<button type="button" class="slick-next"><img src="/img/icon-right.svg"></button>',
+				// eslint-disable-next-line max-len
+				prevArrow: '<button type="button" class="slick-prev"><img src="/img/icon-left.svg"></button>'
 			});
 		});
 	}
