07542a8bb598b1b87b30e42380484361959a6136
Yulian <stryulvas@gmail.com>
2022-09-19 23:33:49 +0300

    main

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@
 $( document ).ready(
 	function() {
 		$( document ).ready( function () {
-			$( '.multiple-items' ).slick({
+			$( '.subslider' ).slick({
 				speed: 500,
 				slidesToShow: 3,
 				slidesToScroll: 3,
@@ -10,6 +10,18 @@ $( document ).ready(
 				nextArrow: '<button type="button" class="slick-next"><img src="/img/icon-right.svg"></button>',
 				// eslint-disable-next-line max-len
 				prevArrow: '<button type="button" class="slick-prev"><img src="/img/icon-left.svg"></button>'
+			});
+
+			$( '.mainslider' ).slick({
+				speed: 500,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				dot: true,
+				arrow:true,
+				// eslint-disable-next-line max-len
+				nextArrow: '<button type="button" class="slick-next"><img src="/img/Chevron-right-large.png"></button>',
+				// eslint-disable-next-line max-len
+				prevArrow: '<button type="button" class="slick-prev"><img src="/img/Chevron-left-large.png"></button>'
 			});
 		});
 	}
